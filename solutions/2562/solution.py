b2d4053cf18e03e78c86982190d9ba68663bd8cf
SeungminAhn <smahn9123@gmail.com>
2024-12-21 09:35:47 +0900

    add more 2562 solutions python
    Signed-off-by: SeungminAhn <smahn9123@gmail.com>

--- solutions/2562/solution.py
+++ solutions/2562/solution.py
@@ -1,11 +1,24 @@
+# Solution 1
+# import sys
+#
+# l = map(int, sys.stdin.readlines())
+# for i, v in enumerate(l, start=1):
+#     if i == 1:
+#         max_v = v
+#         max_i = i
+#     else:
+#         max_v = max(max_v, v)
+#         max_i = i if max_v == v else max_i
+# sys.stdout.write(f"{max_v}\n{max_i}")
+
+# Solution 2
 import sys
 
-l = map(int, sys.stdin.readlines())
-for i, v in enumerate(l, start=1):
-    if i == 1:
-        max_v = v
-        max_i = i
-    else:
-        max_v = max(max_v, v)
-        max_i = i if max_v == v else max_i
-sys.stdout.write(f"{max_v}\n{max_i}")
+l = list(map(int, sys.stdin.readlines()))
+sys.stdout.write(f"{max(l)} {l.index(max(l)) + 1}")
+
+# Solution 3
+# import sys
+#
+# (*l,) = map(int, sys.stdin.readlines())
+# sys.stdout.write(f"{max(l)} {l.index(max(l)) + 1}")
