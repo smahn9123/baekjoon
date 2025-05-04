e2bbc974beb0d2e3512f61b6f62a081e322c723e
Seungmin Ahn <smahn9123@gmail.com>
2025-05-04 10:48:27 +0900

    add 2439 solution go
    Signed-off-by: Seungmin Ahn <smahn9123@gmail.com>

---
+++ solutions/2439/solution.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	for i := 1; i <= n; i++ {
+		for j := 0; j < n-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 0; k < i; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
